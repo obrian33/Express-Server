9eed317147151183e7d84b134d7a6a2dea1d8e5e
O'Brian Santos <obrians@uci.edu>
2018-07-22 12:24:14 -0700

    Added type to service call

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/api.service';
+import { Book } from 'src/app/book.model';
 
 @Component({
   selector: 'app-book',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent implements OnInit {
+  books: Array<Book> = [];
+  book: Book;
 
-  constructor() { }
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.getBooks().subscribe( (books: Array<Book>) => {
+      console.log(this.books);
+      this.books = books;
+    }, error => {
+      console.log(error);
+    });
   }
 
 }
