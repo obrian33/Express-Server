a0fd2d8ebd1fd593b27356dd9579027508e52b2e
O'Brian Santos <obrians@uci.edu>
2018-07-22 11:00:02 -0700

    Added book model and rest service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -14,4 +14,35 @@ const apiUrl = '/api';
 export class ApiService {
 
   constructor(private http: HttpClient) { }
+
+  private handleError = (error: HttpErrorResponse) => {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  }
+
+  private extractData = (res: Response) => {
+    const body = res;
+    return body || { };
+  }
+
+  getBooks = (): Observable<any> => {
+    return this.http.get(apiUrl, httpOptions).pipe(
+      map(this.extractData), catchError(this.handleError));
+  }
+
+  getBookById = (id: string): Observable<any> => {
+    return this.http.get(apiUrl, httpOptions).pipe(
+      map(this.extractData), catchError(this.handleError));
+  }
+
 }
